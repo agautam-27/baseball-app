9647c8a2b1cf7b411ba9e5c646c0768e38cdf948
agautam-27 <122254669+agautam-27@users.noreply.github.com>
2025-03-22 10:56:14 -0700

    fixed logout issue from hosted link-V2

--- frontend/js/player_dashboard.js
+++ frontend/js/player_dashboard.js
@@ -2,17 +2,24 @@ const isHosted = window.location.hostname.includes("netlify.app");
 const basePath = isHosted ? '/components/' : '../components/';
 const loginPath = isHosted ? '/index.html' : '../index.html';
 
+function waitForFirebaseAuthReady() {
+  return new Promise((resolve) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      unsubscribe(); // ensure this only fires once
+      resolve(user); // return user once Firebase is ready
+    });
+  });
+}
 
-firebase.auth().onAuthStateChanged((user) => {
+waitForFirebaseAuthReady().then((user) => {
   if (user) {
     console.log("Authenticated:", user.email);
   } else {
     console.log("No user, redirecting to login...");
-    setTimeout(() => {
-      window.location.href = loginPath;
-    }, 100); 
+    window.location.href = loginPath;
   }
 });
+
 
 function loadSharedUI() {
   fetch(basePath + 'header.html')
